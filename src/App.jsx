4d12a09d544a128228b92bac3ba1d608ecb0ba6d
Silca93 <eric_delhaye@.com>
2024-02-19 14:29:38 +0100

    installation of all modules and frameworks

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+      <div className="card-body bg-red-400 rounded-b-xl">
+      <h2 className="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+       </div>
+       </div>
+    </div>
+      
     </>
   )
 }
