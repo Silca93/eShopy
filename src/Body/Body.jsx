aedccad24c9109eaab1d08288772e15b95fc5bad
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-02-20 14:41:49 +0100

    fixing bg issue and overflow issue

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -8,10 +8,10 @@ export default function Body({data, star, logo, fond}) {
 
 
   return (
-    <div className="body bg-[#222222] w-screen flex flex-col gap-[1rem]">
+    <div className="body bg-[#222222] w-screen overflow-x-hidden flex flex-col gap-[1rem]">
         <Header logo={logo} />
         <Banner fond={fond}/>
-        <div className="cardsDiv w-full h-[170rem] flex justify-center max-[883px]:bg-[#222222] max-[883px]:items-center gap-4 flex-wrap mt-3">
+        <div className="cardsDiv w-full h-[170rem] flex overflow-x-hidden justify-center max-[883px]:bg-[#222222] max-[883px]:items-center max-[500px]:h[100%] max-[500px]:bg-[#222222] gap-4 flex-wrap mt-3">
                   
           {data.map((element, key) => 
           <Card key={key} data={element} star={star}/>
